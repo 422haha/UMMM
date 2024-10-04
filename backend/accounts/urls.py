e9534eac15e1ff13557765d4c53c6d5fd812563c
강미연 <422haha@naver.com>
2024-10-04 21:33:18 +0900

    feat: User 정보 조회 및 수정 API 구현

---
+++ backend/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('<str:username>/', views.user_profile),
+    path('<str:username>/info/', views.user_info),
+    path('<str:username>/profile/', views.user_info_profile),
+]
